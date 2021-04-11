e71fd24c6fc6a51b1a2d6e3af923c3405a669976
Nathan L. Walls <nathan@wallscorp.us>
2021-04-11 11:28:04 -0400

    Point the root route to the entries index

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :entries
   mount Sidekiq::Web => '/sidekiq'
   devise_for :users
+
+  root to: 'entries#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
